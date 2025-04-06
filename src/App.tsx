06e43f5902ce50be6c0972fe3d182e17dec3c885
Antonio <marcoaranibar12@gmail.com>
2025-04-06 10:29:17 -0500

    feat: initialize project with Vite, React, and TypeScript setup
    This commit sets up a new project with Vite, React, and TypeScript configurations. It includes:
    - Initial project structure with `index.html`, `main.tsx`, and `App.tsx`.
    - TypeScript configuration files (`tsconfig.json`, `tsconfig.app.json`, `tsconfig.node.json`).
    - Vite configuration file (`vite.config.ts`).
    - Basic React components (`Header`, `Guitar`, `App`) and types (`Guitar`, `CartItem`).
    - Initial CSS styling and image assets for the project.
    - ESLint and Prettier configurations for code linting and formatting.

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useReducer } from "react";
+import Guitar from "./components/guitar";
+import Header from "./components/header";
+import { cartReducer, initialState } from "./reducers/cart.reducer";
+
+function App() {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
+  return (
+    <>
+      <Header cart={state.cart} dispatch={dispatch} />
+      <main className="mt-5 container-xl">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="mt-5 row">
+          {state.data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} dispatch={dispatch} />
+          ))}
+        </div>
+      </main>
+
+      <footer className="py-5 mt-5 bg-dark">
+        <div className="container-xl">
+          <p className="mt-4 text-center text-white fs-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
